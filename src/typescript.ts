27cc676af6a39c6cfffd805e81acdd4ade8f7bb3
James Prevett <jp@jamespre.dev>
2025-02-06 23:13:59 -0600

    Worked on C->TS boilerplate

--- src/typescript.ts
+++ src/typescript.ts
@@ -12,7 +12,7 @@ function emitType(type: xir.Type, namespace?: string): string {
 		case 'array':
 			return type.length === null
 				? emitType(type.element, namespace) + '[]'
-				: `Tuple<${emitType(type.element, namespace)}, ${type.length}>`;
+				: `ConstArray<${emitType(type.element, namespace)}, ${type.length}>`;
 		case 'ref':
 			return `${type.restricted ? '/* restricted */' : ''} Ref<${emitType(type.to, namespace)}>`;
 		case 'function':
@@ -67,8 +67,49 @@ function emitDecorator(type: xir.Type | null, lenOrNs?: number | null): string {
 	}
 }
 
+/**
+ * Boilerplate to make stuff with C work
+ *
+ * `Ref<T>`
+ *
+ * Pointer to `T`. Note `__ref__` doesn't actually exist and is only here to keep TS types when passing it around
+ *
+ *
+ * `ConstArray<T, L>`
+ *
+ * Constant array `T[L]`.
+ *
+ * `$__ref<T>(value: T): Ref<T>;`
+ *
+ * Equivalent to C `&value`
+ *
+ * `$__deref<T>(value: Ref<T>): T;`
+ *
+ * Equivalent to C `(*value)`
+ *
+ * `$__deref_set<T>(left: Ref<T>, right: Ref<T> | T): void;`
+ *
+ * Equivalent to C `(*value) = ...`
+ *
+ * `$__array<T>(start: Ref<T>, i: bigint): T;`
+ *
+ * Access an element of an array, but using the pointer type (e.g. accessing `x[0]` where `x` is `char*`)
+ *
+ * `$__array<T>(start: Ref<T>, i: bigint, value?: T): void;`
+ *
+ * Set an element of an array, but using the pointer type (e.g. `x[0] = ...` where `x` is `char*`)
+ *
+ * `$__str(value: string): Ref<int8>;`
+ *
+ * Converts a JS string literal into a `char*`/`Ref<int8>`
+ *
+ * `$__allocConstArray<T, L extends number>(length: L, ...init: T[]): ConstArray<T, L>;`
+ *
+ * Allocates an array of length `L` with elements of type `T`. Takes in optional initializers
+ *
+ */
 export const cHeader = `
-import { Tuple, types as t, struct, member, sizeof } from 'utilium';
+import { types as t, struct, member, sizeof } from 'utilium';
 
 type int8 = number;
 type uint8 = number;
@@ -85,13 +126,18 @@ type float64 = number;
 type float128 = number;
 
 type bool = boolean | number;
-type Ref<T> = number;
+type Ref<T> = bigint & { __ref__?: T };
+type ConstArray<T, L extends number> = Array<T> & { length: L } & Ref<T>;
 
-declare function $__assert(condition: boolean, message?: string): void;
+declare function $__assert(condition: boolean, message?: Ref<int8>): void;
 declare function $__ref<T>(value: T): Ref<T>;
 declare function $__deref<T>(value: Ref<T>): T;
 declare function $__array<T>(start: Ref<T>, i: bigint): T | undefined;
 declare function $__array<T>(start: Ref<T>, i: bigint, value?: T): void;
+declare function $__str(value: string): Ref<int8>;
+declare function $__allocConstArray<T, L extends number>(length: L, ...init: T[]): ConstArray<T, L>;
+
+declare let __func__: string | undefined;
 `;
 
 export function emit(u: xir.Unit): string {
