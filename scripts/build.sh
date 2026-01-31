c4b281e60fae14c1cb1e536e72b1a005d2a677e1
James Prevett <jp@jamespre.dev>
2026-01-30 19:26:18 -0600

    Improved directory handling in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Credit: Dave Dopson, https://stackoverflow.com/a/246128/17637456
+project_dir=$(dirname $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))
+
 set -euo pipefail
 
 npx typedoc > /dev/null
 
-cmake-js clean
-cmake-js build
+npx cmake-js -d "$project_dir" clean
+npx cmake-js -d "$project_dir" build
 
-npx tsc -p tsconfig.json
-chmod +x dist/cli.js
+npx tsc -p "$project_dir/tsconfig.json"
+chmod +x "$project_dir/dist/cli.js"
+
+source "$project_dir/node.env"
